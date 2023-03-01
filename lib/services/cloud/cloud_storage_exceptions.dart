4ef1b9a17b4a4337ec39416e35d593801779661b
Dima <xalvas23@gmail.com>
2023-03-01 22:01:53 +0200

    step-19. Writing notes on firebase storage

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,11 @@
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+class CouldNotCreateNewNoteException implements Exception {}
+
+class CouldNotGetAllNotesException implements Exception {}
+
+class CouldNotUpdateNoteException implements Exception {}
+
+class CouldNotDeleteNoteException implements Exception {}
